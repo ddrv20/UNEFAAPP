73952459b261e7426fcd79979dbee6a3bee5145d
Leonardis Rojas <leonardisrojas@gmail.com>
2017-01-05 20:06:17 -0500

    Cambios en el Login

--- app/src/main/java/unefa/app/david/com/unefaapp/Activities/NewUserActivity.java
+++ app/src/main/java/unefa/app/david/com/unefaapp/Activities/NewUserActivity.java
@@ -2,6 +2,7 @@ package unefa.app.david.com.unefaapp.Activities;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -33,6 +34,8 @@ public class NewUserActivity extends AppCompatActivity implements GoogleApiClien
     private TextView mStatusTextView;
     private ProgressDialog mProgressDialog;
 
+    GoogleSignInAccount acct;
+
     Dialogs dialog;
 
 
@@ -54,12 +57,11 @@ public class NewUserActivity extends AppCompatActivity implements GoogleApiClien
                 .requestEmail()
                 .build();
 
+
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
-
-
 
     }
 
@@ -96,7 +98,7 @@ public class NewUserActivity extends AppCompatActivity implements GoogleApiClien
         Log.d(TAG, "handleSignInResult:" + result.isSuccess());
         if (result.isSuccess()) {
             // Signed in successfully, show authenticated UI.
-            GoogleSignInAccount acct = result.getSignInAccount();
+            acct = result.getSignInAccount();
 
             //   mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
             updateUI(true);
@@ -106,10 +108,77 @@ public class NewUserActivity extends AppCompatActivity implements GoogleApiClien
         }
     }
 
+    private String getUserEmail(){
+        SharedPreferences prefs = getSharedPreferences("USER_DATA", MODE_PRIVATE);
+
+        String email = prefs.getString("email", "");
+        return email;
+    }
+
+    private void SavePreferences(String email, String name, String image, String typoUser, String career){
+        SharedPreferences sharedPreferences = getSharedPreferences("USER_DATA", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("email", email);
+        editor.putString("name", name);
+        editor.putString("image", image);
+        editor.putString("typoUser", typoUser);
+        editor.putString("career", career);
+        editor.commit();
+    }
+
+    private boolean userIsNew(String email){
+
+        return true;
+    }
+
     private void updateUI(boolean signedIn) {
         dialog.dismiss();
         if (signedIn) {
+
             findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+
+            if (getUserEmail().equals("") && userIsNew(acct.getEmail())){
+
+                dialog.show();
+
+                dialog.image.setImageDrawable(getResources().getDrawable(R.drawable.students));
+
+                dialog.title.setText("Escoja tipo de usuario");
+                dialog.description.setVisibility(View.GONE);
+                dialog.layoutProgress.setVisibility(View.GONE);
+
+                dialog.spinnerTypoUser.setVisibility(View.VISIBLE);
+
+                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(NewUserActivity.this,
+                        R.array.typo_user_array, android.R.layout.simple_spinner_item);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                dialog.spinnerTypoUser.setAdapter(adapter);
+
+                dialog.layoutButtons.setVisibility(View.VISIBLE);
+                dialog.layoutButtonNegative.setVisibility(View.GONE);
+                dialog.layoutAux.setVisibility(View.GONE);
+                dialog.positiveText.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+
+                        SavePreferences(acct.getEmail(), acct.getDisplayName(),
+                                acct.getPhotoUrl().toString(),
+                                dialog.spinnerTypoUser.getSelectedItem().toString(),
+                                dialog.spinnerCareer.getSelectedItem().toString());
+                        Intent mainIntent = new Intent().setClass(NewUserActivity.this, TypoUserActivity.class);
+                        startActivity(mainIntent);
+                        finish();
+                    }
+                });
+            }else {
+                SavePreferences(acct.getEmail(), acct.getDisplayName(),
+                        acct.getPhotoUrl().toString(),
+                        "",
+                        "");
+                Intent mainIntent = new Intent().setClass(NewUserActivity.this, TypoUserActivity.class);
+                startActivity(mainIntent);
+            }
 
             //  findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
         } else {
@@ -117,31 +186,7 @@ public class NewUserActivity extends AppCompatActivity implements GoogleApiClien
 
             findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
 
-            dialog.show();
 
-            dialog.image.setImageDrawable(getResources().getDrawable(R.drawable.students));
-
-            dialog.title.setText("Escoja tipo de usuario");
-            dialog.description.setVisibility(View.GONE);
-            dialog.layoutProgress.setVisibility(View.GONE);
-
-            dialog.spinnerTypoUser.setVisibility(View.VISIBLE);
-
-            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(NewUserActivity.this,
-                    R.array.typo_user_array, android.R.layout.simple_spinner_item);
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            dialog.spinnerTypoUser.setAdapter(adapter);
-
-            dialog.layoutButtons.setVisibility(View.VISIBLE);
-            dialog.layoutButtonNegative.setVisibility(View.GONE);
-            dialog.layoutAux.setVisibility(View.GONE);
-            dialog.positiveText.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent mainIntent = new Intent().setClass(NewUserActivity.this, TypoUserActivity.class);
-                    startActivity(mainIntent);
-                }
-            });
             //findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
         }
     }
@@ -149,14 +194,14 @@ public class NewUserActivity extends AppCompatActivity implements GoogleApiClien
     private void signIn() {
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
-        /*dialog.show();
+        dialog.show();
 
         dialog.image.setImageDrawable(getResources().getDrawable(R.drawable.students));
 
         dialog.title.setText("Iniciando Sesion");
         dialog.description.setVisibility(View.GONE);
         dialog.layoutProgress.setVisibility(View.VISIBLE);
-        dialog.progressText.setText("Cargando!!!");*/
+        dialog.progressText.setText("Cargando!!!");
 
         /*final TimerTask task = new TimerTask() {
             @Override
